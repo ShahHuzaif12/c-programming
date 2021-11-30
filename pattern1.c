74ecf34edacdb741c5c62ad5f6eb995937c847db
ShahHuzaif12 <93786360+ShahHuzaif12@users.noreply.github.com>
2021-11-30 22:51:24 +0530

    Add files via upload

--- pattern1.c
+++ pattern1.c
@@ -2,14 +2,16 @@
 int main()
 {
     int i, j, n;
-    printf("Enter the number of the rows: \n");
+    printf("Enter the number of the rows\n");
     scanf("%d", &n);
-    for(i = 0; i<n; i++)
+
+    for (i=1; i<=n; i++)
     {
-        for(j=0; j<i; j++)
+        for(j=1; j<=i; j++)
         {
             printf("* ");
-        }
-        printf("\n");
+        }  
+         printf("\n");
     }
+    
 }
